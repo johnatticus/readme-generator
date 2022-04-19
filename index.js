010e0787fe29ad6c20ff636a109f0acbcf9bce12
John Hysong <johnahysong@gmail.com>
2022-04-19 17:49:18 -0400

    added questions

--- index.js
+++ index.js
@@ -11,7 +11,57 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Enter a short description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license does your project have?',
+        choices: ['None', 'Apache License 2.0', 'MIT', 'Mozilla Public License 2.0', 'GNU General Public License v2.0'],
+    },
+    {
+        type: 'input',
+        message: 'What command should I run to install dependencies?',
+        name: 'dependencies',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run for tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about said project?',
+        name: 'whattoknow',
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to this repo?',
+        name: 'contribution',
+    },
+])
+}
 
 // Function call to initialize app
 init();

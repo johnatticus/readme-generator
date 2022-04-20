b8ba533590f380f636616cfdbe1b6dcffb41968d
John Hysong <johnahysong@gmail.com>
2022-04-19 21:28:43 -0400

    added license badge and license link code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return '[[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  if (license === "APACHE 2.0") {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  if (license === "GPL 3.0") {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  if (license === "BSD 3") {
+    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  }
+  if (license === "None") {
+    return '[![License]("")'
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license !== "None") {
+      return `### License
+      
+      This project is licensed under ${license}.`;
+    }
+  // if (license === "MIT") {
+  //   return '\n * [License](https://opensource.org/licenses/MIT)'
+  // }
+  // if (license === "APACHE 2.0") {
+  //   return '\n * [License](https://opensource.org/licenses/Apache-2.0)'
+  // }
+  // if (license === "GPL 3.0") {
+  //   return '\n * [License](https://www.gnu.org/licenses/gpl-3.0)'
+  // }
+  // if (license === "BSD 3") {
+  //   return '\n * [License](https://opensource.org/licenses/BSD-3-Clause)'
+  // }
+  // if (license === "None") {
+  //   return '\n * [License](#)'
+  // }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string

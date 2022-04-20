3ef515c69f39b34308089ecd1e69f4bee7068715
John Hysong <johnahysong@gmail.com>
2022-04-19 21:33:02 -0400

    did not save file before previous push. oops.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -49,7 +49,9 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.project}
+
+  
 
 `;
 }
